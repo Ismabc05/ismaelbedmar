ebee41f853335bb2858b866df1ae4c5db7780c55
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-02-09 14:35:57 +0100

    inicio

---
+++ portfolio/src/components/Inicio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../estilos/inicio.css';
+
+function Inicio() {
+    return(
+        <section>
+            <h1>Ismael Bedmar</h1>
+        </section>
+    )
+}
+
+
+
+
+export { Inicio }
