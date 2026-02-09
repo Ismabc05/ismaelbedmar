8bb77e35c8ff04e280c8456af0ba5259f3bb7c2b
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-02-09 13:20:16 +0100

    estilos-navbar

---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../estilos/navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">Ismael Bedmar</div>
+            <ul className="lista-navbar">
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#sobremi">Sobre mi</a></li>
+                <li><a href="#servicios">Servicios</a></li>
+                <li><a href="#proyectos">Proyectos</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+
+export { Navbar } ;
